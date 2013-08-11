d08fae27061da5a25a8a6ab17e14313d9caf7e88
bryn austin bellomy <bryn.bellomy@gmail.com>
2013-08-11 06:05:03 -0400

    this is rifuckingdiculous

--- travis/script.sh
+++ travis/script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-#set -e
+set -e
 
 WORKSPACE_FILENAME=./GCDThreadsafe.xcworkspace
 SCHEME_NAME=GCDThreadsafe
@@ -8,8 +7,8 @@ SCHEME_NAME=GCDThreadsafe
 echo "===========> Doing 'build test' command (no -sdk flag)"
 xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" build test
 
-echo "===========> Doing 'run-tests' command (no -sdk flag)"
-xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" run-tests
+#echo "===========> Doing 'run-tests' command (no -sdk flag)"
+#xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" run-tests
 
 #xctool -workspace ./GCDThreadsafe.xcworkspace -scheme GCDThreadsafe -sdk  build test
 
