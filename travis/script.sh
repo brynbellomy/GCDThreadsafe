3d5bf025d3cec74392b47b44781de7c66a882b6f
bryn austin bellomy <bryn.bellomy@gmail.com>
2013-08-11 05:26:19 -0400

    travis

--- travis/script.sh
+++ travis/script.sh
@@ -4,7 +4,11 @@ set -e
 WORKSPACE_FILENAME=./GCDThreadsafe.xcworkspace
 SCHEME_NAME=GCDThreadsafe
 
-xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" -sdk iphonesimulator build test
+echo "===========> Doing 'build test' command (no -sdk flag)"
+xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" build test
+
+echo "===========> Doing 'run-tests' command (no -sdk flag)"
+xctool -workspace "$WORKSPACE_FILENAME" -scheme "$SCHEME_NAME" run-tests
 
 #xctool -workspace ./GCDThreadsafe.xcworkspace -scheme GCDThreadsafe -sdk  build test
 
